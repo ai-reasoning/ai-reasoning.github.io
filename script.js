6df5033c14ca3d1dce20f621274ae171ff7f2423
jolene <jolenechong7@gmail.com>
2025-06-01 15:11:51 +0700

    added sticky positioning to navbar

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function toggleMenu() {
     const menu = document.getElementById("mobile-menu");
     menu.classList.toggle("hidden");
-  }
+}
+
+document.querySelectorAll('#mobile-menu a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.getElementById('mobile-menu').classList.add('hidden');
+    });
+});
+
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector("nav");
+    if (window.scrollY > 0) {
+      navbar.classList.add("sticky", "top-0", "z-50", "shadow-md");
+    } else {
+      navbar.classList.remove("sticky", "top-0", "z-50", "shadow-md");
+    }
+});
